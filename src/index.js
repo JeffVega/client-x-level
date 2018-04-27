bad790e3d2a4bedc7dd6f8f1f1247c8397523da8
Jeff Vega <jeff@intellibliss.com>
2018-04-27 15:25:52 -0700

    added the store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Main from './components/main';
+import {Provider} from 'react-redux';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Main />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
